37a86ba24193a4cb727f5c83e0d880e1f7a46843
HamCraft <dhedhiahmedyaqoob@gmail.com>
2025-11-29 15:07:08 +0500

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\dhedh\\Downloads\\phys-main\\phys-main\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\dhedh\\Downloads\\phys-main\\phys-main\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\dhedh\\Downloads\\phys-main\\phys-main\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\dhedh\\Downloads\\phys-main\\phys-main\\src\\css\\custom.css'),
+];
